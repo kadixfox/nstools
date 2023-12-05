84af6ff08eb66a570dc19f88a73a979c382467a9
kadix <kadix@p12.lan>
2023-12-05 00:09:07 -0700

    start skeleton for ember.sh

---
+++ phoenix/ember.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## NATIONSTATES V BABYFUR!?!??!?!?!!!!?!?!?!!??11??!?
+## are we on part 3 now? part 4? i lost track >w<
+##
+## the game of cat and mouse; a game that never ends
+## 
+## -= kadix =-
+##
+## ember.sh - create nations
+##
+## Dec 5 2023, 00:05 MST
+
+agent="really cool browser useragent"
+
+apiagent="really cool api useragent"
